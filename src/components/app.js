771a5f83adf061616b16a255454bff1c1fb67c1e
Dennis A. Seidel <mail@dennisseidel.de>
2016-05-02 09:33:58 +0200

    Add redux and eslint and create searchbar

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import { Component } from 'react';
+import React, { Component } from 'react';
+// import components
+import SearchBar from '../containers/search-bar';
 
+// Root node rendering my app
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <SearchBar />
+      </div>
     );
   }
 }
